032ab42c3a712684b98b4d23090f3f0928b79297
madalina-lavinia-chelu <madalina.lavinia.chelu@gmail.com>
2024-01-07 20:09:01 +0200

    add code with Supplier

--- Java 1 - Professional/Programare Functionala/FunctionalInterface/src/main/java/Main.java
+++ Java 1 - Professional/Programare Functionala/FunctionalInterface/src/main/java/Main.java
@@ -1,7 +1,17 @@
+import java.util.function.*;
+
 public class Main {
     public static void main(String[] args) {
+        /*
         MyLogger logger = new MyLogger();
         logger.print("The first test");
+         */
+
+        //Nu primește nici un parametru și are rolul de a genera o valoar
+        Supplier <Double> randomNumber = () -> Math.random() * 100;
+        System.out.println("Random number " + randomNumber.get());
+
+
 
     }
 }
